70a30fafc81535604f6344f05773603df4faf73a
Rob <snowjunkierob@gmail.com>
2021-09-02 17:51:15 -0600

    modified new products route to correctly send request to client server and then to new api

--- client/src/components/ProductOverview/ProductOverviewAPIUtils.js
+++ client/src/components/ProductOverview/ProductOverviewAPIUtils.js
@@ -1,58 +1,59 @@
 import axios from 'axios';
+// import { response } from 'express';
 
 export default {
   getProductAndStyles(id) {
-    return new Promise((resolve, reject) => {
-      axios.get(`http://localhost:3000/products/product_id=${id}`)
-        .then((response) => {
 
-          resolve(response.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  },
-  getProduct(id) {
     return new Promise((resolve, reject) => {
-      axios
-        .get(`/api/products/${47422}`)
+      axios.get(`/api/products/${id}`)
         .then(response => {
+          // console.log(response)
           resolve(response.data);
         })
-        .catch(err => {
-          reject(err);
-        });
+        .catch(err => {});
     });
   },
-
-  getProductStyles(id) {
-    return new Promise((resolve, reject) => {
-      axios
-        .get(`/api/products/${47422}/styles`)
-        .then(response => {
-          resolve(response.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  },
-
-  getReviews(id) {
-    return new Promise((resolve, reject) => {
-      axios
-        .get(`/api/reviews/${id}/relevence`)
-        .then(response => {
-          resolve(response.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  },
+  // getReviews(id) {
+  //   return new Promise((resolve, reject) => {
+  //     axios
+  //       .get(`/api/reviews/${id}/relevence`)
+  //       .then(response => {
+  //         resolve(response.data);
+  //       })
+  //       .catch(err => {
+  //         reject(err);
+  //       });
+  //   });
+  // },
 
   sendClickData(clickData) {
     return axios.post('/api/interactions/clickData', clickData);
   },
 };
+
+//old routes
+// getProduct(id) {
+//   return new Promise((resolve, reject) => {
+//     axios
+//       .get(`/api/products/${47422}`)
+//       .then(response => {
+//         resolve(response.data);
+//       })
+//       .catch(err => {
+//         reject(err);
+//       });
+//   });
+// },
+
+// getProductStyles(id) {
+//   return new Promise((resolve, reject) => {
+//     axios
+//       .get(`/api/products/${47422}/styles`)
+//       .then(response => {
+//         resolve(response.data);
+//       })
+//       .catch(err => {
+//         reject(err);
+//       });
+//   });
+// },
--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,34 +1,47 @@
 const axios = require('axios');
 
-const getProduct = (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.productId}`, {
-    headers: {
-      'Authorization': process.env.TOKEN
-    }
-  })
-    .then(response => {
+const getProductAndStyles = (req, res) => {
+  // console.log(req.params.productId)
+  axios.get(`http://localhost:3000/products/product_id=${req.params.productId}`)
+    .then((response) => {
+
       res.json(response.data);
     })
     .catch(err => {
-      console.error(err);
+      // reject(err);
     });
 };
 
-const getProductStyles = (req, res) => {
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.productId}/styles`, {
-    headers: {
-      'Authorization': process.env.TOKEN
-    }
-  })
-    .then(response => {
-      res.json(response.data);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-};
+// const getProduct = (req, res) => {
+//   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.productId}`, {
+//     headers: {
+//       'Authorization': process.env.TOKEN
+//     }
+//   })
+//     .then(response => {
+//       res.json(response.data);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
+// };
+
+// const getProductStyles = (req, res) => {
+//   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${req.params.productId}/styles`, {
+//     headers: {
+//       'Authorization': process.env.TOKEN
+//     }
+//   })
+//     .then(response => {
+//       res.json(response.data);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//     });
+// };
 
 module.exports = {
-  getProduct,
-  getProductStyles
+  // getProduct,
+  // getProductStyles,
+  getProductAndStyles
 };

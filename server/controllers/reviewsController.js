387fe50d129924c7a006282ae2d43053abeb737c
Sunik Kim <sunik.h.kim@gmail.com>
2021-09-04 14:57:18 -0400

    post new review route almost integrated with API

--- server/controllers/reviewsController.js
+++ server/controllers/reviewsController.js
@@ -104,63 +104,12 @@ const postNewReview = async (req, res) => {
       data.photos = fileURLs;
     }
 
-    console.log('data to post', data);
-
     const response = await axios.post('http://localhost:8080/reviews', data, headers);
     res.sendStatus(201);
   } catch (error) {
     res.send(error);
   }
 };
-
-// const postNewReview = async (req, res) => {
-//   try {
-//     const characteristics = {};
-
-//     req.body.characteristics.forEach(characteristic => {
-//       const charData = characteristic.split('-');
-//       characteristics[parseInt(charData[0])] = parseInt(charData[1]);
-//     });
-
-//     const data = {
-//       product_id: parseInt(req.body.product_id),
-//       rating: parseInt(req.body.rating),
-//       summary: req.body.summary,
-//       body: req.body.body,
-//       recommend: req.body.recommend === 'Yes' ? true : false,
-//       name: req.body.name,
-//       email: req.body.email,
-//       characteristics: characteristics
-//     };
-//     const headers = {
-//       headers: {
-//         Authorization: process.env.TOKEN
-//       }
-//     };
-
-//     if (req.files.length) {
-//       const files = req.files.map((file, index) => {
-//         return new Promise(async (resolve, reject) => {
-//           const base64string = file.buffer.toString('base64');
-//           const options = {
-//             apiKey: process.env.IMG_API_KEY,
-//             base64string
-//           };
-//           const url = await imgbbUploader(options);
-//           resolve(url.image.url);
-//         });
-//       });
-
-//       const fileURLs = await Promise.all(files);
-//       data.photos = fileURLs;
-//     }
-
-//     const response = await axios.post('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/reviews', data, headers);
-//     res.sendStatus(201);
-//   } catch (error) {
-//     res.send(error);
-//   }
-// };
 
 module.exports = {
   getReviews,
